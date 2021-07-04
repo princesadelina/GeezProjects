fdb4aebfe34ca54f46b2677054699b9e9b58707c
Geez <79180058+vckyou@users.noreply.github.com>
2021-07-05 01:38:52 +0700

    Update calls.py

--- userbot/modules/calls.py
+++ userbot/modules/calls.py
@@ -11,7 +11,7 @@ from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
 from telethon.tl.types import ChatAdminRights
-from userbot import CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.events import geezbot_cmd
 from userbot import CUSTOM_CMD as geez
 
