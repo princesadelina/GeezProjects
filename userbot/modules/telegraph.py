352bc6279cdf0b844f65819a5dc7adc742226d29
Geez <79180058+vckyou@users.noreply.github.com>
2021-07-05 06:08:32 +0700

    Update telegraph.py

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -7,78 +7,99 @@
 #
 
 import os
+from datetime import datetime
 
-from telegraph import Telegraph
-from telegraph import upload_file as uf
+from PIL import Image
+from telegraph import Telegraph, exceptions, upload_file
 
-from userbot import CMD_HELP, bot, ALIVE_NAME
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import geezbot_cmd
 from userbot import CUSTOM_CMD as geez
 
-
-# Telegraph Things
 telegraph = Telegraph()
-try:
-    telegraph.create_account(short_name=ALIVE_NAME)
-
-except BaseException:
-    telegraph.create_account(short_name="Geez")
-
-_copied_msg = {}
+r = telegraph.create_account(short_name="telegraph")
+auth_url = r["auth_url"]
 
 
-@bot.on(geezbot_cmd(outgoing=True, pattern="tg"))
-async def _(event):
-    input_str = str(event.pattern_match.group(1))
-    if event.reply_to_msg_id:
-        getmsg = await event.get_reply_message()
-        if getmsg.photo or getmsg.video or getmsg.gif:
-            getit = await bot.download_media(getmsg)
-            try:
-                variable = uf(getit)
-                os.remove(getit)
-                nn = "https://telegra.ph" + variable[0]
-                amsg = f"Uploaded to [Telegraph]({nn}) !"
-            except Exception as e:
-                amsg = f"Error - {e}"
-            await event.edit(amsg)
-        elif "pic" in mediainfo(getmsg.media):
-            getit = await bot.download_media(getmsg)
-            try:
-                variable = uf(getit)
-                os.remove(getit)
-                nn = "https://telegra.ph" + variable[0]
-                amsg = f"Uploaded to [Telegraph]({nn}) !"
-            except Exception as e:
-                amsg = f"Error - {e}"
-            await event.edit(amsg)
-        elif getmsg.document:
-            getit = await bot.download_media(getmsg)
-            ab = open(getit)
-            cd = ab.read()
-            ab.close()
-            if input_str:
-                tcom = input_str
-            else:
-                tcom = "Geez"
-            makeit = telegraph.create_page(title=tcom, content=[f"{cd}"])
-            war = makeit["url"]
-            os.remove(getit)
-            await event.edit(f"Pasted to Telegraph : [Telegraph]({war})")
-        elif getmsg.text:
-            if input_str:
-                tcom = input_str
-            else:
-                tcom = "Geez"
-            makeit = telegraph.create_page(
-                title=tcom, content=[f"{getmsg.text}"])
-            war = makeit["url"]
-            await event.edit(f"Pasted to Telegraph : [Telegraph]({war})")
+@bot.on(geezbot_cmd(outgoing=True, pattern="tg (t|m)(?: |$)(.*)"))
+async def telegraphs(graph):
+    await graph.edit("`Sedang Memproses...`")
+    if not graph.text[0].isalpha() and graph.text[0] not in (
+            "/", "#", "@", "!"):
+        if graph.fwd_from:
+            return
+        if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+            os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+        if graph.reply_to_msg_id:
+            start = datetime.now()
+            r_message = await graph.get_reply_message()
+            input_str = graph.pattern_match.group(1)
+            if input_str == "m":
+                downloaded_file_name = await bot.download_media(
+                    r_message, TEMP_DOWNLOAD_DIRECTORY
+                )
+                end = datetime.now()
+                ms = (end - start).seconds
+                await graph.edit(
+                    "Di Download Ke {} Dalam {} Detik.".format(downloaded_file_name, ms)
+                )
+                try:
+                    if downloaded_file_name.endswith((".webp")):
+                        resize_image(downloaded_file_name)
+                except AttributeError:
+                    return await graph.edit("`Tidak Ada Media Yang Disediakan`")
+                try:
+                    start = datetime.now()
+                    media_urls = upload_file(downloaded_file_name)
+                except exceptions.TelegraphException as exc:
+                    await graph.edit("ERROR: " + str(exc))
+                    os.remove(downloaded_file_name)
+                else:
+                    end = datetime.now()
+                    ms_two = (end - start).seconds
+                    os.remove(downloaded_file_name)
+                    await graph.edit(
+                        "Berhasil Mengunggah Ke [Telegraph](https://telegra.ph{}).".format(
+                            media_urls[0], (ms + ms_two)
+                        ),
+                        link_preview=True,
+                    )
+            elif input_str == "t":
+                user_object = await bot.get_entity(r_message.from_id)
+                title_of_page = user_object.first_name  # + " " + user_object.last_name
+                # apparently, all Users do not have last_name field
+                page_content = r_message.message
+                if r_message.media:
+                    if page_content != "":
+                        title_of_page = page_content
+                    downloaded_file_name = await bot.download_media(
+                        r_message, TEMP_DOWNLOAD_DIRECTORY
+                    )
+                    m_list = None
+                    with open(downloaded_file_name, "rb") as fd:
+                        m_list = fd.readlines()
+                    for m in m_list:
+                        page_content += m.decode("UTF-8") + "\n"
+                    os.remove(downloaded_file_name)
+                page_content = page_content.replace("\n", "<br>")
+                response = telegraph.create_page(
+                    title_of_page, html_content=page_content
+                )
+                end = datetime.now()
+                ms = (end - start).seconds
+                await graph.edit(
+                    "Berhasil Mengunggah Ke [Telegraph](https://telegra.ph/{}).".format(
+                        response["path"], ms
+                    ),
+                    link_preview=True,
+                )
         else:
-            await event.edit("Reply to a Media or Text !")
-    else:
-        await event.edit("Reply to a Message !")
+            await graph.edit("`Mohon Balas Ke Pesan, Untuk Mendapatkan Link Telegraph Permanen.`")
 
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
 
 CMD_HELP.update({"telegraph": f">`{geez}tg`"
                  "\nUsage: Upload (text) or (media) on Telegraph."})
