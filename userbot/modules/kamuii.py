8b8ff684e42c613955dda3369fb4264013632e2f
Geez <79180058+vckyou@users.noreply.github.com>
2021-07-05 03:14:25 +0700

    Update kamuii.py

--- userbot/modules/kamuii.py
+++ userbot/modules/kamuii.py
@@ -10,7 +10,7 @@ from userbot import CUSTOM_CMD as geez
 from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 
 
-@bot.on(geezbot_cmd(outgoing=True, pattern=r"kamuii(: ? |$)([1 - 8])?')
+@bot.on(geezbot_cmd(outgoing=True, pattern=r'^.kamuii(:? |$)([1-8])?'))
 async def _(fry):
     await fry.edit("`Prosess, Mengaktifkan jurus tumor bolong...😜`")
     level=fry.pattern_match.group(2)
