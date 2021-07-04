94f2d303cea0e8c6ee87080127bb5f580a39cab8
Geez <79180058+vckyou@users.noreply.github.com>
2021-07-05 01:26:47 +0700

    Create calls.py

---
+++ userbot/modules/calls.py
@@ -0,0 +1,91 @@
+# Thanks Full To Team Ultroid
+# Ported By Vcky @VckyouuBitch
+# Copyright (c) 2021 Geez - Projects
+# Geez - Projects https://github.com/Vckyou/Geez-UserBot
+# Ini Belum Ke Fix Ya Bg :')
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from telethon.tl.types import ChatAdminRights
+from userbot import CMD_HELP
+from userbot.events import geezbot_cmd
+from userbot import CUSTOM_CMD as geez
+
+NO_ADMIN = "`Sorry you are not admin :)`"
+
+
+async def get_call(event):
+    geez = await event.client(getchat(event.chat_id))
+    vcky = await event.client(getvc(geez.full_chat.call))
+    return vcky.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i: i + n]
+
+
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"startvc$"))
+async def _(e):
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        return await e.edit(NO_ADMIN)
+    new_rights = ChatAdminRights(invite_users=True)
+    try:
+        await e.client(startvc(e.chat_id))
+        await e.edit("`Voice Chat Started...`")
+    except Exception as ex:
+        await e.edit(f"`{str(ex)}`")
+
+
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"stopvc$"))
+async def _(e):
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        return await e.edit(NO_ADMIN)
+    new_rights = ChatAdminRights(invite_users=True)
+    try:
+        await e.client(stopvc(await get_call(e)))
+        await e.edit("`Voice Chat Stopped...`")
+    except Exception as ex:
+        await e.edit(f"`{str(ex)}`")
+
+
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"stopvc$"))
+async def _(e):
+    await e.edit("`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in e.client.iter_participants(e.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await e.client(invitetovc(call=await get_call(e), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await e.edit(f"`Invited {z} users`")
+
+
+CMD_HELP.update(
+    {
+        "calls": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{geez}startvc`\
+         \n↳ : Start Group Call in a group.\
+         f\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{geez}stopvc`\
+         \n↳ : `Stop Group Call in a group.`\
+         f\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{geez}vcinvite`\
+         \n↳ : Invite all members of group in Group Call. (You must be joined)."
+    }
+)
