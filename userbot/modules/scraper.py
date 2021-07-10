525780a05b089525be4f6f850dcbd309fa5deaa0
Geez <79180058+vckyou@users.noreply.github.com>
2021-07-10 22:26:15 +0700

    okey

---
+++ userbot/modules/scraper.py
@@ -0,0 +1,80 @@
+import asyncio
+import csv
+import random
+import traceback
+
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserPrivacyRestrictedError,
+    UserNotMutualContactError
+)
+
+from userbot.events import geezbot_cmd
+from userbot import CUSTOM_CMD as geez
+from userbot import CMD_HELP, bot
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+
+
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"getmemb ?(.*)"))
+async def scrapmem(event):
+    chat = event.chat_id
+    y = await event.edit("`Please wait...`")
+    client = event.client
+    members = await client.get_participants(chat, aggressive=True)
+
+    with open("members.csv", "w", encoding="UTF-8") as f:
+        writer = csv.writer(f, delimiter=",", lineterminator="\n")
+        writer.writerow(["user_id", "hash"])
+        for member in members:
+            writer.writerow([member.id, member.access_hash])
+    await event.edit("`Members scraped.`")
+
+
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"addmemb ?(.*)"))
+async def admem(event):
+    x = await event.edit("`Adding 0 members...`")
+    chat = await event.get_chat()
+    client = event.client
+    users = []
+    with open("members.csv", encoding="UTF-8") as f:
+        rows = csv.reader(f, delimiter=",", lineterminator="\n")
+        next(rows, None)
+        for row in rows:
+            user = {'id': int(row[0]), 'hash': int(row[1])}
+            users.append(user)
+    n = 0
+    for user in users:
+        n += 1
+        if n % 30 == 0:
+            await event.edit(f"`Mencapai 30 anggota, tunggu selama {900/60} menit`")
+            await asyncio.sleep(900)
+        try:
+            userin = InputPeerUser(user['id'], user['hash'])
+            await client(InviteToChannelRequest(chat, [userin]))
+            await asyncio.sleep(random.randrange(5, 7))
+            await event.edit(f"`Menambahkan {n} anggota...`")
+        except TypeError:
+            n -= 1
+            continue
+        except UserAlreadyParticipantError:
+            n -= 1
+            continue
+        except UserPrivacyRestrictedError:
+            n -= 1
+            continue
+        except UserNotMutualContactError:
+            n -= 1
+            continue
+
+
+CMD_HELP.update({
+    "scraper":
+    f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{geez}getmemb`\
+   \nUsage : Mengumpulkan Anggota dari Obrolan\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{geez}addmemb`\
+   \nUsage : Menambahkan Anggota ke Obrolan\
+   \nTata Cara Menggunakannya:  Pertama, Anda harus melakukan {geez}getmemb terlebih dahulu dari Obrolan. Lalu buka grup Anda dan ketik {geez}addmemb untuk menambahkan mereka ke grup Anda."
+
+   
+})
