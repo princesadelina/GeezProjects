120b472733e9a097ecf4725bea5bc6b175df6edd
princesadelina <88875263+princesadelina@users.noreply.github.com>
2021-08-13 16:16:25 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -89,7 +89,7 @@ async def pingme(pong):
     await pong.edit("`Pinging....`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**PONG!! 🏓**\n**Pinger** : %sms\n**Bot Uptime** : {uptime}🕛" % (duration))
+    await pong.edit(f"**Unicorn!! 👸🏻🦄**\n**Belle 🌻** : %sms\n**Snowhite🍎** : {uptime}🕛" % (duration))
 
 
 @bot.on(geezbot_cmd(outgoing=True, pattern="pong$"))
